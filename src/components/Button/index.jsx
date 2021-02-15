039599f4290dbba9342cfac55c73a6ced8ed8817
Caique Giovannini <caique_giovannini@hotmail.com>
2021-02-15 18:46:42 -0300

    add button types

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,26 +4,35 @@ import PropTypes from 'prop-types';
 import './styles.css';
 
 const Button = ({
-  isSubmit, onClick, children, className,
-}) => (
-  <button
-    className={className}
-    type={isSubmit ? 'submit' : 'button'}
-    onClick={onClick}
-  >
-    {children}
-  </button>
-);
+  isSubmit, onClick, children, type,
+}) => {
+  const buttonTypes = {
+    default: '',
+    add: 'button--add',
+    transparent: 'button--transparent',
+  };
+
+  return (
+    <button
+      className={`button ${buttonTypes[type]}`}
+      type={isSubmit ? 'submit' : 'button'}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 Button.defaultProps = {
   isSubmit: null,
+  type: 'default',
 };
 
 Button.propTypes = {
   isSubmit: PropTypes.string,
   children: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  className: PropTypes.string.isRequired,
+  type: PropTypes.string,
 };
 
 export default Button;
