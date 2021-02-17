720e96178324aa5de94ec176d7408af3a5c8b7e0
Caique Giovannini <caique_giovannini@hotmail.com>
2021-02-17 12:46:41 -0300

    finalize add new tool modal

--- src/components/AddNewTool/index.jsx
+++ src/components/AddNewTool/index.jsx
@@ -2,16 +2,42 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Input from '../Input';
+import Textarea from '../Textarea';
+import Button from '../Button';
 
 import { ReactComponent as CloseIcon } from '../../assets/icons/close.svg';
 
-import './AddNewTool.css';
+import './styles.css';
 
 const AddNewTool = ({ onClose }) => {
-  const [toolName, setToolName] = useState();
+  const [toolName, setToolName] = useState('');
+  const [toolLink, setToolLink] = useState('');
+  const [toolDescription, setToolDescription] = useState('');
+  const [tags, setTags] = useState('');
+
+  function clearAllFields() {
+    setToolName('');
+    setToolLink('');
+    setToolDescription('');
+    setTags('');
+  }
+
+  function closeModal() {
+    clearAllFields();
+    onClose(false);
+  }
 
   function handleAddTool(event) {
     event.preventDefault();
+
+    const tagsArray = tags.length > 0 ? tags.split(' ') : [];
+
+    console.log({
+      toolName,
+      toolLink,
+      toolDescription,
+      tagsArray,
+    });
   }
 
   return (
@@ -19,19 +45,50 @@ const AddNewTool = ({ onClose }) => {
       <div className="modal">
         <header className="modal__header">
           <h4>+ Add new tool</h4>
-          <button type="button" onClick={() => onClose(false)}>
+
+          <Button
+            onClick={closeModal}
+            type="transparent"
+          >
             <CloseIcon style={{ width: '1rem', height: '1rem' }} />
-          </button>
+          </Button>
         </header>
 
-        <form onSubmit={handleAddTool}>
+        <form className="modal__form" onSubmit={handleAddTool}>
           <Input
             id="name"
             label="Tool name"
             value={toolName}
-            onChange={setToolName}
+            onChange={(event) => setToolName(event.target.value)}
           />
-          <button type="submit" className="button">Add tool</button>
+          <Input
+            id="link"
+            label="Tool link"
+            placeholder="https://"
+            value={toolLink}
+            onChange={(event) => setToolLink(event.target.value)}
+          />
+          <Textarea
+            id="description"
+            label="Tool description"
+            value={toolDescription}
+            onChange={(event) => setToolDescription(event.target.value)}
+          />
+          <Input
+            id="tags"
+            label="Tags"
+            placeholder="separate tags by space"
+            value={tags}
+            onChange={(event) => setTags(event.target.value)}
+          />
+          <div className="modal__form__button">
+            <Button
+              type="add"
+              isSubmit
+            >
+              Add tool
+            </Button>
+          </div>
         </form>
       </div>
     </section>
