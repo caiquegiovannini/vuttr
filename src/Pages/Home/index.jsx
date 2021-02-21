bbbdd56b7c24d736c14e09374024fc3c785e50a8
Caique Giovannini <caique_giovannini@hotmail.com>
2021-02-21 17:47:12 -0300

    add external api and fetch the data at home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Tool from '../../components/Tool';
 import AddNewTool from '../../components/AddNewTool';
@@ -7,39 +8,52 @@ import Button from '../../components/Button';
 
 import './Home.css';
 
-const tools = [
-  {
-    id: 1,
-    title: 'Notion',
-    link: 'https://notion.so',
-    description:
-      'All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ',
-    tags: ['organization', 'planning', 'collaboration', 'writing', 'calendar'],
-  },
-  {
-    id: 2,
-    title: 'json-server',
-    link: 'https://github.com/typicode/json-server',
-    description:
-      'Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.',
-    tags: ['api', 'json', 'schema', 'node', 'github', 'rest'],
-  },
-  {
-    id: 3,
-    title: 'fastify',
-    link: 'https://www.fastify.io/',
-    description:
-      'Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.',
-    tags: ['web', 'framework', 'node', 'http2', 'https', 'localhost'],
-  },
-];
-
 const Home = () => {
+  const allToolsMock = [
+    {
+      id: 1,
+      title: 'Notion',
+      link: 'https://notion.so',
+      description:
+        'All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ',
+      tags: ['organization', 'planning', 'collaboration', 'writing', 'calendar'],
+    },
+    {
+      id: 2,
+      title: 'json-server',
+      link: 'https://github.com/typicode/json-server',
+      description:
+        'Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.',
+      tags: ['api', 'json', 'schema', 'node', 'github', 'rest'],
+    },
+    {
+      id: 3,
+      title: 'fastify',
+      link: 'https://www.fastify.io/',
+      description:
+        'Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.',
+      tags: ['web', 'framework', 'node', 'http2', 'https', 'localhost'],
+    },
+  ];
+  const [allTools, setAllTools] = useState();
+  const [listedTools, setListedTools] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function handleAddNewTool() {
     setModalIsOpen(true);
   }
+
+  useEffect(() => {
+    async function fetchAllTolls() {
+      const response = await axios.get('http://localhost:8080/tools');
+      const data = await response.data;
+
+      setAllTools(data);
+      setListedTools(data);
+    }
+
+    fetchAllTolls();
+  }, [setAllTools, setListedTools]);
 
   return (
     <section className="container">
@@ -52,7 +66,7 @@ const Home = () => {
         <div className="actions__search">
           <Input
             id="actions__search"
-            placeholder="search"
+            placeholder="search by title or tag"
             value=""
             onChange={() => { }}
           />
@@ -75,9 +89,9 @@ const Home = () => {
         </Button>
       </div>
 
-      {tools && (
-        <section className="tools-list">
-          {tools.map((tool) => <Tool key={tool.id} tool={tool} />)}
+      {listedTools && (
+        <section className="tools-list" data-testid="tools-list">
+          {listedTools.map((tool) => <Tool key={tool.id} tool={tool} />)}
         </section>
       )}
 
