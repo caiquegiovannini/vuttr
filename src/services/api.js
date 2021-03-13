a2f5793bc7c37fb11f30c9c645ba30b675feee45
Caique Giovannini <caique_giovannini@hotmail.com>
2021-03-13 16:09:31 -0300

    add remove tool to api

--- src/services/api.js
+++ src/services/api.js
@@ -10,3 +10,4 @@ const api = axios.create({
 
 export const getTools = async () => (await api.get(baseURL)).data;
 export const addTool = async (tool) => (await api.post(baseURL, tool)).data;
+export const removeTool = async (id) => (await api.delete(`${baseURL}/${id}`)).data;
